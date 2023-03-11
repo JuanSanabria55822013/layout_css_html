6c1139010685e9db9e25c9f167c7fb00478e2d4c
Sala D14 <jssanabriam@uniboyaca.edu.co>
2023-03-11 09:55:07 -0500

    Js para saludar

---
+++ js/saludar.js
@@ -0,0 +1,12 @@
+// document.getElementById('nombre')
+//     .setAttribute('value', 'Johan');
+
+document.getElementById('saludarBtn')
+    .addEventListener('click', () => {
+        let nombrePersona = document.getElementById('nombre').value;
+        if(nombrePersona == '' || nombrePersona == null){
+            alert('Debe ingresar el nombre')
+        } else {
+            alert('Hola '+nombrePersona+'!!!!!!!!')
+        }
+});
