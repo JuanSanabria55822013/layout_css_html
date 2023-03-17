23af3b1caf68fb4c8b3b8c34ace03f101751615f
Sala D8 <CSalaD8@uniboyaca.edu.co>
2023-03-16 19:22:31 -0500

    Cambios en el js

--- js/saludar.js
+++ js/saludar.js
@@ -4,9 +4,18 @@
 document.getElementById('saludarBtn')
     .addEventListener('click', () => {
         let nombrePersona = document.getElementById('nombre').value;
-        if(nombrePersona == '' || nombrePersona == null){
+        if(!nombrePersona == '' || nombrePersona == null){
             alert('Debe ingresar el nombre')
         } else {
             alert('Hola '+nombrePersona+'!!!!!!!!')
         }
 });
+
+function saludar(){
+    let nombrePersona = document.getElementById('nombre')
+    if(!nombrePersona){
+        alert('Debe ingresar el nombre')
+    } else {
+        alert('Hola '+nombrePersona+'!!!!!!!!')
+    }
+}
